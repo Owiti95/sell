ee543ed2f874b57a81240c49e39c571810a57a05
hannington owiti <hanningtonowiti@gmail.com>
2024-09-22 21:30:48 +0300

    establishing a connection with sales.db and defining session factory

--- database.py
+++ database.py
@@ -1,7 +1,17 @@
+#a database.py file to establish a connection with the sales.db by creating an engine and defining a session factory to manage database transactions
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 
 DATABASE_URL = "sqlite:///sales.db"
 
 engine = create_engine(DATABASE_URL, echo=True)
-Session = sessionmaker(bind=engine)
+# 'echo=True' enables SQL query logging for debugging purposes
+# Creating a new SQLAlchemy engine, which will be used to interact with the database
+
+
+#create a session factory attached to the engine
+Session = sessionmaker(bind=engine)#interact with the database and perform CRUD operations
+
+
